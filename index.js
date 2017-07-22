f61490f9e1a6967ab75754f0a6186f07846514c5
Antony Jones <aj@desirableobjects.co.uk>
2017-07-22 12:58:33 +0100

    generate config

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
 'use strict'
 
+const { bootstrap } = require('./lib/cli')
 
-// const { join } = require('path')
-
-//   const cwd = process.cwd()
-//   const config = require(join(cwd, path))
-
-const scrypt = require('scrypt-async')
-const complex = '!@#$%^&*()_+{}:"<>?\|[];\',./`~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('')
-const simple = complex.slice(31)
-
-function convert (char, arr) {
-  const index = char > arr.length ? char % arr.length : char
-  return arr[index]
-}
-
-scrypt('http://www.dogsbody.io', '2granularangular50', {
-    N: 16384,
-    r: 8,
-    p: 1,
-    dkLen: 32,
-    encoding: 'binary'
-}, function(derivedKey) {
-  let s = ''
-  derivedKey.forEach(next => {
-    const char = convert(next, simple)
-    return s += char
-  })
-
-  console.log('Simple:', s)
+bootstrap().then(cli => {
+  cli.parse(process.argv)
 })
