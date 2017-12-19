daea7718f6bd8ad999b6052fc03763693e810490
Antony Jones <aj@desirableobjects.co.uk>
2017-12-19 11:29:14 +0000

    add initial site

---
+++ site/main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import $ from 'jquery'
+
+window.jQuery = $
+
+import 'bootstrap'
+//import 'owl.carousel2'
+// import './js/script'
+// import './css/style.css'
+// import './css/themify-icons.css'
